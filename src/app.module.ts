445309a1f4df567e268278358832ae6be6544700
novascript-dev <novaisbusycoding@gmail.com>
2025-01-23 13:24:37 -0300

    feat: add healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { StripeModule } from './stripe/stripe.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingMiddleware } from './middleware/logging.middleware';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [StripeModule],
+  controllers: [HealthController],
   providers: [
     {
       provide: APP_PIPE,
